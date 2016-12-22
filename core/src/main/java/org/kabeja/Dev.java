6f8feb597d11a1a00d6956c5501da18f987ae031
Benjamin Aaron Degenhart <benjamin.degenhart@gmail.com>
2016-12-22 12:45:15 +0100

    WIP

--- core/src/main/java/org/kabeja/Dev.java
+++ core/src/main/java/org/kabeja/Dev.java
@@ -1,6 +1,15 @@
 package org.kabeja;
 
+import org.kabeja.common.DraftEntity;
+import org.kabeja.common.Layer;
+import org.kabeja.common.Type;
+import org.kabeja.dxf.parser.DXFParser;
+import org.kabeja.dxf.parser.DXFParserBuilder;
 import org.kabeja.dxf.parser.SAXDXFParserBuilder;
+import org.kabeja.entities.LW2DVertex;
+import org.kabeja.entities.LWPolyline;
+import org.kabeja.entities.Viewport;
+import org.kabeja.parser.ParseException;
 import org.kabeja.parser.Parser;
 
 import java.io.BufferedInputStream;
@@ -8,6 +17,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Collection;
+import java.util.HashMap;
 
 import static java.nio.file.Files.newInputStream;
 
@@ -16,14 +27,42 @@ import static java.nio.file.Files.newInputStream;
  */
 public class Dev {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
 
-        Path devFile = Paths.get("C:\\Users\\benjaminaaron\\git\\postvis\\testfiles\\dxf\\dev.dxf");
+        Path devFile = Paths.get("samples/Drawing1-qcad.dxf");
         //System.out.println(Files.exists(devFile));
 
         try (BufferedInputStream bis = new BufferedInputStream(newInputStream(devFile))){
 
-            Parser parser = SAXDXFParserBuilder.buildFromStream(bis);
+
+            Parser parser = DXFParserBuilder.createDefaultParser();
+            DraftDocument doc = parser.parse(bis, new HashMap<>());
+
+
+            for (Viewport viewport : doc.getViewports()) {
+                System.out.println(viewport.getID());
+            }
+
+            Collection<Layer> layers = doc.getLayers();
+            for (Layer layer : layers) {
+                System.out.println(layer.getName());
+
+                /*for (Type<? extends DraftEntity> type : layer.getEntityTypes()) {
+                    System.out.println(type.getName());
+                }*/
+
+                if (!layer.isFrozen()) {
+
+                    for (LWPolyline line : layer.getEntitiesByType(Type.TYPE_LWPOLYLINE)) {
+
+                        for (LW2DVertex vertex : line.getVertices()) {
+                            System.out.println(vertex.getPoint().toString());
+                        }
+                    }
+                }
+
+            }
+
 
         } catch (IOException e) {
             e.printStackTrace();
